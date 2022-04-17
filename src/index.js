d0e868823e0f3e28e6d524a1a8568d608b30c262
kaushikbhat07 <kaushik.bhat11@learner.manipal.edu>
2022-04-17 15:42:52 +0530

    Add firebase analytics

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 // import ReactDOM from "react-dom/client";
 // import "./index.css";
 import reportWebVitals from "./reportWebVitals";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { firebaseConfig } from "./firebaseConfig";
 
 // ReactDOM.createRoot(document.getElementById("root"));
 // root.render(
@@ -8,6 +11,11 @@ import reportWebVitals from "./reportWebVitals";
 //         <App />
 //     </React.StrictMode>
 // );
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// eslint-disable-next-line no-unused-vars
+const analytics = getAnalytics(app);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
